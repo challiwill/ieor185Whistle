6bbfaac31b2b2926f80f03b332052d6b2123a635
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2015-05-06 13:16:37 -0700

    automatically refresh for new messages in conversationView

--- ChatApp/conversationVC.swift
+++ ChatApp/conversationVC.swift
@@ -69,6 +69,8 @@ class conversationVC: UIViewController, UIScrollViewDelegate, UITextViewDelegate
         let tapScrollViewGesture = UITapGestureRecognizer(target: self, action: "didTapScrollView")
         tapScrollViewGesture.numberOfTapsRequired = 1
         resultsScrollView.addGestureRecognizer(tapScrollViewGesture)
+
+        var refreshTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "refreshResults", userInfo: nil, repeats: true)
     }
     
     func didTapScrollView() {
